f5234fcd37769d698676d9f31fcae982194bed2b
Raneet Roy <raneetroy20@gmail.com>
2023-04-15 22:48:20 +0530

    Added files

---
+++ js/music-list.js
@@ -0,0 +1,116 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Courtesy Call",
+    artist: "Thousand Foot Krutch",
+    img: "roy-1",
+    src: "roy-1"
+  },
+  {
+    name: "My Demons",
+    artist: "Starset",
+    img: "roy-2",
+    src: "roy-2"
+  },
+  {
+    name: "Time of Our Lives",
+    artist: "Ahmed Chawki",
+    img: "roy-3",
+    src: "roy-3"
+  },
+  {
+    name: "Mohombi",
+    artist: "Say Jambo",
+    img: "roy-4",
+    src: "roy-4"
+  },
+  {
+    name: "Numb",
+    artist: "Linkin Park",
+    img: "roy-5",
+    src: "roy-5"
+  },
+  {
+    name: "AC : Valhalla",
+    artist: "Nerdout",
+    img: "roy-6",
+    src: "roy-6"
+  },
+  {
+    name: "Experience - LM10",
+    artist: "Peter Drury",
+    img: "roy-7",
+    src: "roy-7"
+  },
+  {
+    name: "Dil Dooba",
+    artist: "Sonu Nigam",
+    img: "roy-8",
+    src: "roy-8"
+  },
+  {
+    name: "Imaginary",
+    artist: "Imran Khan",
+    img: "roy-9",
+    src: "roy-9"
+  },
+  {
+    name: "Kaun Tujhe",
+    artist: "Armaan Malik",
+    img: "roy-10",
+    src: "roy-10"
+  },
+  {
+    name: "Superman",
+    artist: "Eminem",
+    img: "roy-11",
+    src: "roy-11"
+  },
+  {
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    img: "roy-12",
+    src: "roy-12"
+  },
+  {
+    name: "Bilionera",
+    artist: "Otilia",
+    img: "roy-13",
+    src: "roy-13"
+  },
+  {
+    name: "Let Me Love You",
+    artist: "DJ Snake ft. Justin Bieber",
+    img: "roy-14",
+    src: "roy-14"
+  },
+  {
+    name: "Safari",
+    artist: "Serena",
+    img: "roy-15",
+    src: "roy-15"
+  },
+  {
+    name: "Tonight (I'm Lovin' You)",
+    artist: "Enrique Iglesias",
+    img: "roy-16",
+    src: "roy-16"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
